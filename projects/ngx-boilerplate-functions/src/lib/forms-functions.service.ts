1bdd40c9d92f80e667d45a52445fc3fa181d180d
Fredrick <fredabisai@gmail.com>
2024-07-28 18:27:46 +0300

    Add setFormFieldsValidations function to set validations

--- projects/ngx-boilerplate-functions/src/lib/forms-functions.service.ts
+++ projects/ngx-boilerplate-functions/src/lib/forms-functions.service.ts
@@ -1,9 +1,23 @@
 import {Injectable} from "@angular/core";
+import {FormControl, FormGroup, UntypedFormGroup, ValidatorFn} from "@angular/forms";
+import {VERSION} from "@angular/cli";
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormsFunctionsService {
+  private majorVersion = parseInt(VERSION.major, 10);
 
   constructor() { }
+  setFormFieldsValidations(form: FormGroup | UntypedFormGroup, fields: {name: string, validations: ValidatorFn[]}[]): void {
+    if (form && fields?.length) {
+      for (const field of fields) {
+        if ( field?.name &&  form?.contains(field.name) && field?.validations?.length) {
+          form.get(field.name)!.setValidators(field.validations);
+          form.get(field.name)!.updateValueAndValidity();
+        }
+      }
+    }
+  }
+
 }
