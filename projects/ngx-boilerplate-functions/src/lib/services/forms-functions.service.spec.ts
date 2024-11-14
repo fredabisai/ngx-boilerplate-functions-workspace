e3e54a5d5b87a821e30c982586a0be6b949eb65c
Fredrick <fredabisai@gmail.com>
2024-11-14 10:53:48 +0300

    Create unit tests for setFormFieldsValidations

---
+++ projects/ngx-boilerplate-functions/src/lib/services/forms-functions.service.spec.ts
@@ -0,0 +1,84 @@
+import { TestBed } from '@angular/core/testing';
+
+import { FormsFunctionsService } from './forms-functions.service';
+import {FormBuilder, FormGroup, UntypedFormGroup, Validators} from "@angular/forms";
+import {FormFieldInfo} from "../interfaces/ngx-boilerplate-functions.interface";
+
+describe('FormsFunctionsService', () => {
+  let service: FormsFunctionsService;
+  let form: FormGroup;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(FormsFunctionsService);
+    form = new FormBuilder().group({
+      name: [''],
+      email: [''],
+      age: ['']
+    });
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+  it('[setFormFieldsValidations]: should set validators on specified form fields', () => {
+    const fields: FormFieldInfo[] = [
+      { name: 'name', validations: [Validators.required] },
+      { name: 'email', validations: [Validators.email, Validators.required] }
+    ];
+
+    service.setFormFieldsValidations(form, fields);
+
+    expect(form.get('name')!.hasValidator(Validators.required)).toBeTrue();
+    expect(form.get('email')!.hasValidator(Validators.email)).toBeTrue();
+    expect(form.get('email')!.hasValidator(Validators.required)).toBeTrue();
+  });
+
+  it('[setFormFieldsValidations]: should not change validations for fields not listed', () => {
+    const initialValidator = form.get('age')!.validator;
+
+    service.setFormFieldsValidations(form, [{ name: 'name', validations: [Validators.required] }]);
+
+    expect(form.get('age')!.validator).toBe(initialValidator);
+  });
+
+  it('[setFormFieldsValidations]: should handle fields that do not exist in the form', () => {
+    const fields: FormFieldInfo[] = [
+      { name: 'nonExistentField', validations: [Validators.required] }
+    ];
+
+    service.setFormFieldsValidations(form, fields);
+
+    expect(form.contains('nonExistentField')).toBeFalse();
+  });
+
+  it('[setFormFieldsValidations]: should skip fields with empty or undefined validations array', () => {
+    const fields: FormFieldInfo[] = [
+      { name: 'name', validations: [] }
+    ];
+
+    service.setFormFieldsValidations(form, fields);
+
+    expect(form.get('name')!.validator).toBeNull();
+  });
+
+  it('[setFormFieldsValidations]: should update value and validity after setting validators', () => {
+    const fields: FormFieldInfo[] = [
+      { name: 'name', validations: [Validators.required] }
+    ];
+
+    spyOn(form.get('name')!, 'updateValueAndValidity');
+
+    service.setFormFieldsValidations(form, fields);
+
+    expect(form.get('name')!.updateValueAndValidity).toHaveBeenCalled();
+  });
+
+  it('[setFormFieldsValidations]: should not throw errors if form is null', () => {
+    expect(() => service.setFormFieldsValidations(null as any, [])).not.toThrow();
+  });
+
+  it('[setFormFieldsValidations]: should not throw errors if fields array is empty', () => {
+    expect(() => service.setFormFieldsValidations(form, [])).not.toThrow();
+  });
+});
