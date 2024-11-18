ba75d066d79d78e81ebe46094872bf23c6eb6427
Fredrick <fredabisai@gmail.com>
2024-11-18 15:56:37 +0300

    COmment field class

--- projects/ngx-boilerplate-functions/src/lib/interfaces/ngx-boilerplate-functions.interface.ts
+++ projects/ngx-boilerplate-functions/src/lib/interfaces/ngx-boilerplate-functions.interface.ts
@@ -15,42 +15,42 @@ export interface IFormFieldInfo {
   defaultValue?: any;
   value?: any;
   options?: { onlySelf?: boolean; emitEvent?: boolean;};
-  formatType?: 'string' | 'number' | 'float' | 'boolean' | 'date' | 'datetime';
+  formatType?:  'string' | 'number' | 'float' | 'boolean' | 'date' |  'remove' | 'add';
   dateFormat?: string;
   mappedKey?: string;
 }
-export class FormFieldInfo implements IFormFieldInfo {
-  name: string;
-  validations?: ValidatorFn[];
-  defaultValue?: any;
-  value?: any;
-  options?: { onlySelf?: boolean; emitEvent?: boolean;};
-  formatType?: 'string' | 'number' | 'float' | 'boolean' | 'date' |  'remove' | 'add';
-  dateFormat?: string;
-  mappedKey?: string;
-}
+// export class FormFieldInfo implements IFormFieldInfo {
+//   name: string;
+//   validations?: ValidatorFn[];
+//   defaultValue?: any;
+//   value?: any;
+//   options?: { onlySelf?: boolean; emitEvent?: boolean;};
+//   formatType?: 'string' | 'number' | 'float' | 'boolean' | 'date' |  'remove' | 'add';
+//   dateFormat?: string;
+//   mappedKey?: string;
+// }
 export interface IFormsFunctionsService {
-  setFormGroupValidations(form: FormGroup | UntypedFormGroup, fields: FormFieldInfo[]): void;
-  removeFormGroupValidations(form: FormGroup | UntypedFormGroup, fields: FormFieldInfo[]): void;
-  addAndRemoveFieldsOnSubmission(form: FormGroup | UntypedFormGroup, fieldsToAdd: FormFieldInfo[] = [],
-                                 fieldsToRemove: string[] = [] ): any;
-  disableFields(form: FormGroup | UntypedFormGroup, fieldsToDisable: FormFieldInfo[]): void;
-  patchValuesToFields(form: FormGroup | UntypedFormGroup, fieldsToSet: FormFieldInfo[]): void;
-  changeFormControlFields(form: FormGroup | UntypedFormGroup, fieldsToAdd: FormFieldInfo[],
+  setFormGroupValidations(form: FormGroup | UntypedFormGroup, fields: IFormFieldInfo[]): void;
+  removeFormGroupValidations(form: FormGroup | UntypedFormGroup, fields: IFormFieldInfo[]): void;
+  addAndRemoveFieldsOnSubmission(form: FormGroup | UntypedFormGroup, fieldsToAdd: IFormFieldInfo[],
+                                 fieldsToRemove: string[]): any;
+  disableFields(form: FormGroup | UntypedFormGroup, fieldsToDisable: IFormFieldInfo[]): void;
+  patchValuesToFields(form: FormGroup | UntypedFormGroup, fieldsToSet: IFormFieldInfo[]): void;
+  changeFormControlFields(form: FormGroup | UntypedFormGroup, fieldsToAdd: IFormFieldInfo[],
                           fieldsToRemove: {name: string, emitEvent?: boolean}[]): void;
   checkIfFormControlsMatch( formGroup: FormGroup | UntypedFormGroup, controlName: string,
                             matchingControlName: string): void;
-  initializeFormGroup(formBuilder: FormBuilder | UntypedFormBuilder, fields: FormFieldInfo[]): FormGroup | undefined;
-  resetFormGroup(formGroup: FormGroup | UntypedFormGroup, defaultFields?: FormFieldInfo[]): void;
+  initializeFormGroup(formBuilder: FormBuilder | UntypedFormBuilder, fields: IFormFieldInfo[]): FormGroup | undefined;
+  resetFormGroup(formGroup: FormGroup | UntypedFormGroup, defaultFields?: IFormFieldInfo[]): void;
   getFormControlErrorMessage(control: FormControl | UntypedFormControl,
                              errorType:  'required' | 'requiredTrue' | 'minLength' | 'maxLength'
                                | 'pattern' | 'min' | 'max' | 'email'): any;
-  isFormControlValidWithControlMark(control: FormControl, controlMarks: ('dirty' | 'pristine' | 'touched')[]): boolean;
+  isFormControlValidWithControlMark(control: FormControl, controlMarks: ('dirty' | 'pristine' | 'touched')[]): boolean | undefined;
   isFormGroupValid(forGroup: FormGroup | UntypedFormGroup): boolean;
   getFormGroupErrorMessages(forGroup: FormGroup | UntypedFormGroup): {key: string[]} | undefined;
-  getFormPayloadForSubmission(formGroup: FormGroup | UntypedFormGroup, fieldsToFormat: FormFieldInfo[]);
+  getFormPayloadForSubmission(formGroup: FormGroup | UntypedFormGroup, fieldsToFormat: IFormFieldInfo[]): any;
   patchFormGroupValues(formGroup: FormGroup | UntypedFormGroup, data: any,
-                       mappedKeys?: FormFieldInfo[]): FormGroup | UntypedFormGroup;
+                       mappedKeys?: IFormFieldInfo[]): FormGroup | UntypedFormGroup;
   markAllControlsAsTouched(form: FormGroup | UntypedFormGroup): void;
   addFormControl(form: FormGroup | UntypedFormGroup, controlName: string,
                  control: FormControl | UntypedFormGroup): void;
